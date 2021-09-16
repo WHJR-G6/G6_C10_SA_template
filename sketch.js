516fbf70cf04ae4f5b3ff34c3d436eef44d88070
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-09-17 01:01:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,23 @@
+var player;
+var platform;
+var player_image, platform_image;
 
 function preload(){
+  player_image=loadImage("images/jack2.png")
+  platform_image = loadImage("images/p1.png");
 }
 
 function setup() {
   createCanvas(500, 500);
- 
+  player = createSprite(250,250,20,50);
 
+  platform = createSprite(250,350,100,10);
+  platform.addImage("platform", platform_image)
+  platform.scale=0.15
 }
 
 function draw() {
   background('#F2F5B6');
-
+  
   drawSprites();
 }
-
-
-
-
-
-
-
